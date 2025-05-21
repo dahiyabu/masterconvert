f3e48ee5fcb08a16fe92bd0370e5a04e8715d0a4
giloys <info@giloys.com>
2025-05-21 19:30:38 +0000

    GS and 7z changes

--- services/converter-sw/compress.py
+++ services/converter-sw/compress.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import mimetypes
 import logging
@@ -77,6 +78,9 @@ def compress_image(input_file, output_file, quality_level):
         output_file (str): Path to the output image file.
         quality_level (str): 'low', 'medium', or 'high'.
     """
+    if not os.path.exists(input_file):
+        logger.info("File not found Not Compressing image=%s",input_file)
+        return
     input_ext = input_file.split('.')[-1].lower()
     output_ext = output_file.split('.')[-1].lower()
 
@@ -94,7 +98,6 @@ def compress_image(input_file, output_file, quality_level):
         except ValueError:
             print("Invalid quality level. Using default (85).")
             compression_quality = 85
-
     command = [config.FFMPEG_PATH, '-i', input_file, '-q:v', str(compression_quality), output_file]
 
     try:
@@ -121,7 +124,7 @@ def compress_pdf(input_file, output_file, quality_level):
     else:
         quality = '/default'
 
-    command = ['gs', '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+    command = [config.GS_PATH, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
                '-dPDFSETTINGS=' + quality, '-dNOPAUSE', '-dQUIET', '-dBATCH',
                '-sOutputFile=' + output_file, input_file]
     subprocess.run(command, check=True, capture_output=True)
@@ -148,7 +151,7 @@ def compress_generic(input_file, output_file, quality_level):
          compression_level = '5'
 
     # 7z doesn't like overwriting without -y
-    command = ['7z', 'a', '-t7z', '-mx=' + compression_level, '-y', output_file, input_file]
+    command = [config.SEVENZ_PATH, 'a', '-t7z', '-mx=' + compression_level, '-y', output_file, input_file]
     try:
         subprocess.run(command, check=True, capture_output=True)
         logger.info("File compressed successfully using 7z.")
@@ -164,6 +167,9 @@ def compress_file(input_file,output_file, quality_level):
         file_path (str): The path to the file to compress.
         quality_level (str): The desired quality level ('low', 'medium', 'high', or a bitrate).
     """
+    if not os.path.exists(input_file):
+        logger.error("File not found=%s",input_file)
+        return
     try:
         if input_file.lower().endswith(('.mp4', '.mov', '.avi', '.mkv', '.webm')):
             compress_video(input_file, output_file, quality_level)
--- services/converter-sw/config.py
+++ services/converter-sw/config.py
@@ -20,16 +20,22 @@ if SYSTEM_PLATFORM == 'windows':
     TESSERACT_PATH = os.path.join(BASE_PATH, 'dependencies', 'tesseract-ocr','tesseract.exe')
     FFMPEG_PATH = os.path.join(BASE_PATH, 'dependencies', 'ffmpeg.exe')
     TESSDATA_DIR = os.path.join(BASE_PATH,'dependencies','tesseract-ocr')
+    GS_PATH = os.path.join(BASE_PATH,'dependencies','gswin64c.exe')
+    SEVENZ_PATH = os.path.join(BASE_PATH,'dependencies','7z.exe')
 elif SYSTEM_PLATFORM == 'darwin':  # macOS
     LIBREOFFICE_PATH = os.path.join(BASE_PATH, 'dependencies', 'LibreOffice.app', 'Contents', 'MacOS', 'soffice')
     TESSERACT_PATH = os.path.join(BASE_PATH, 'dependencies', 'tesseract')
     FFMPEG_PATH = os.path.join(BASE_PATH, 'dependencies', 'ffmpeg')
     TESSDATA_DIR = os.path.join(BASE_PATH,'dependencies')
+    GS_PATH = os.path.join(BASE_PATH,'dependencies','gs')
+    SEVENZ_PATH = os.path.join(BASE_PATH,'dependencies','7z')
 elif SYSTEM_PLATFORM == 'linux':
     LIBREOFFICE_PATH = os.path.join(BASE_PATH, 'dependencies', 'libreoffice')
     TESSERACT_PATH = os.path.join(BASE_PATH, 'dependencies', 'tesseract')
     FFMPEG_PATH = os.path.join(BASE_PATH, 'dependencies', 'ffmpeg')
     TESSDATA_DIR = os.path.join(BASE_PATH,'dependencies')
+    GS_PATH = os.path.join(BASE_PATH,'dependencies','gs')
+    SEVENZ_PATH = os.path.join(BASE_PATH,'dependencies','7z')
 else:
     raise Exception(f"Unsupported platform: {SYSTEM_PLATFORM}")
 
