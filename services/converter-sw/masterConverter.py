6560ae1e4fa634b423e9e958107f9545dd0054a7
giloys <info@giloys.com>
2025-05-24 02:45:40 +0000

    Update masterConverter.py

--- services/converter-sw/masterConverter.py
+++ services/converter-sw/masterConverter.py
@@ -1041,10 +1041,9 @@ if __name__ == '__main__':
     cleanup_files(get_base_folder())
     set_console_title("MasterConverter")
     ready_file=os.path.join(get_base_folder(),'app_ready.tmp')
-    print(ready_file)
     with open(ready_file, "w") as f:
         f.write("ready")
     setup()
     print("Enjoy Master Convert at http://127.0.0.1:5000 in your browser")
-    print("To Close,\n Press Ctrl+C")
+    print("To Close, Close the window")
     app.run(debug=True,use_reloader=False, host='0.0.0.0', port=5000)
