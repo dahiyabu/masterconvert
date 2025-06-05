e525a98bcbef4703fd5cb86a62e5cf69fc2a11d7
giloys <info@giloys.com>
2025-06-05 19:32:15 +0000

    version change

---
+++ services/converter-sw/version.py
@@ -0,0 +1,40 @@
+from PyInstaller.utils.win32.versioninfo import (
+    FixedFileInfo,
+    StringFileInfo,
+    StringTable,
+    StringStruct,
+    VarFileInfo,
+    VarStruct,
+    VSVersionInfo
+)
+
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 0, 0, 0),
+    prodvers=(1, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+  ),
+  kids=[
+    StringFileInfo([
+      StringTable(
+        '040904B0',
+        [
+          StringStruct('CompanyName', 'ConvertMaster'),
+          StringStruct('FileDescription', 'File Format Converter Tool'),
+          StringStruct('FileVersion', '1.0.0.0'),
+          StringStruct('InternalName', 'convertMaster.exe'),
+          StringStruct('LegalCopyright', '(c) 2025 ConvertMaster'),
+          StringStruct('OriginalFilename', 'convertMaster.exe'),
+          StringStruct('ProductName', 'ConvertMaster'),
+          StringStruct('ProductVersion', '1.0.0.0')
+        ]
+      )
+    ]),
+    VarFileInfo([VarStruct('Translation', [1033, 1200])])
+  ]
+)
