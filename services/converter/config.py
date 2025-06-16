d4cacffc6ef712672f5c92d3240e97fa9c1d7055
VD <>
2025-06-16 23:41:38 +0000

    util changes

--- services/converter/config.py
+++ services/converter/config.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 import os
 import platform
@@ -30,29 +31,34 @@ elif SYSTEM_PLATFORM == 'darwin':  # macOS
     RAR_PATH = os.path.join(BASE_PATH, 'dependencies', 'rar', 'rar')
     UNRAR_PATH = os.path.join(BASE_PATH, 'dependencies', 'rar', 'unrar')
 elif SYSTEM_PLATFORM == 'linux':
-    LIBREOFFICE_PATH = os.path.join(BASE_PATH, 'dependencies', 'libreoffice')
-    TESSERACT_PATH = os.path.join(BASE_PATH, 'dependencies', 'tesseract')
-    FFMPEG_PATH = os.path.join(BASE_PATH, 'dependencies', 'ffmpeg')
+    LIBREOFFICE_PATH = shutil.which('libreoffice') or os.path.join(BASE_PATH, 'dependencies', 'libreoffice')
+    TESSERACT_PATH = shutil.which('tesseract') or os.path.join(BASE_PATH, 'dependencies', 'tesseract')
+    FFMPEG_PATH = shutil.which('ffmpeg') or os.path.join(BASE_PATH, 'dependencies', 'ffmpeg')
     TESSDATA_DIR = os.path.join(BASE_PATH,'dependencies','tesseract','tessdata')
-    GS_PATH = os.path.join(BASE_PATH,'dependencies','gs')
-    SEVENZ_PATH = os.path.join(BASE_PATH,'dependencies','7z')
-    RAR_PATH = os.path.join(BASE_PATH, 'dependencies', 'rar', 'rar')
-    UNRAR_PATH = os.path.join(BASE_PATH, 'dependencies', 'rar', 'unrar')
+    GS_PATH = shutil.which('gs') or os.path.join(BASE_PATH,'dependencies','gs')
+    SEVENZ_PATH = shutil.which('7z') or os.path.join(BASE_PATH,'dependencies','7z')
+    RAR_PATH = shutil.which('rar') or os.path.join(BASE_PATH, 'dependencies', 'rar', 'rar')
+    UNRAR_PATH = shutil.which('unrar') or os.path.join(BASE_PATH, 'dependencies', 'rar', 'unrar')
 else:
     raise Exception(f"Unsupported platform: {SYSTEM_PLATFORM}")
 
 # Set the TESSDATA_PREFIX environment variable to point to tessdata directory
-os.environ['TESSDATA_PREFIX'] = TESSDATA_DIR + os.sep
+if not shutil.which('tesseract'):
+    os.environ['TESSDATA_PREFIX'] = TESSDATA_DIR + os.sep
 
 # Ensure dependencies exist
 def check_dependencies():
     dependencies = {
         'LibreOffice': LIBREOFFICE_PATH,
         'Tesseract': TESSERACT_PATH,
-        'FFmpeg': FFMPEG_PATH
+        'FFmpeg': FFMPEG_PATH,
+        'Ghostscript': GS_PATH,
+        '7zip': SEVENZ_PATH,
+        'RAR': RAR_PATH,
+        'UnRAR': UNRAR_PATH,
     }
     for dep_name, dep_path in dependencies.items():
-        if not os.path.exists(dep_path):
+        if not dep_path or not os.path.exists(dep_path):
             logger.info(f"{dep_name} not found at {dep_path}")
         else:
             logger.info(f"{dep_name} found at {dep_path}")
